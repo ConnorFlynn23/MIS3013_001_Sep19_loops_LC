e938fbfcefacb52d7623fff508a1dfd8110fcdf0
cflyn <cflyn@DESKTOP-JL7F1U5>
2022-09-19 18:25:44 -0500

    Add project files.

---
+++ MIS3013_001_Sep19_loops_LC/Program.cs
@@ -0,0 +1,30 @@
+﻿//Connor Flynn
+//MIS 3013 001 Sept 19 LC over loops
+
+
+// simple: int bool char double
+//complex: string
+
+// counter accumulator
+int totalNumInt;
+
+totalNumInt = 0;
+
+totalNumInt = totalNumInt + 1;
+totalNumInt++;
+totalNumInt += 1;
+
+
+totalNumInt = totalNumInt + 100;
+totalNumInt += 100;
+
+double balDbl = 0;
+
+balDbl = balDbl + 50.50;
+balDbl = balDbl + 1.0;
+balDbl += 50;
+
+
+
+
+Console.ReadLine();
